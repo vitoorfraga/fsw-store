84098235fae6b6bacc9342b1c325891da20be2af
Vitor Fraga <vitorfragaps@gmail.com>
2023-10-21 12:48:42 -0300

    feat: add second banner home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -29,8 +29,19 @@ export default async function Home() {
       </section>
 
       <section className="mt-8">
+        <p className="font-bold uppercase pl-5 mb-3">Ofertas</p>
         <ProducList products={deals} />
       </section>
+
+
+      <Image
+        className="h-auto w-full p-5"
+        sizes="100vw"
+        src="/banner_home_02.png"
+        alt="Até 55% de desconto em mouses"
+        width={0}
+        height={0}
+      />
     </main>
   )
 }
